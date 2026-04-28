16756fde9ebd60304dcbf62e99b638d1360a2f9d
rizvi <abdur-rehman.rizvi@globaldws.com>
2026-04-28 10:17:39 -0400

    requirements.txt

--- controllers.py
+++ controllers.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, UploadFile, File, Form, HTTPException
+from fastapi.responses import JSONResponse 
 from time import ctime
 
 from face_service import (
     register_person,
     recognize_person,
-    verify_person,
     list_persons,
     delete_person
 )
@@ -21,7 +21,8 @@ def health():
 @router.post("/register")
 def register_face(person_id: str = Form(...), image: UploadFile = File(...)):
     try:
-        return register_person(person_id, image)
+        result = register_person(person_id, image)
+        return JSONResponse(content=result, status_code=201)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -29,23 +30,26 @@ def register_face(person_id: str = Form(...), image: UploadFile = File(...)):
 @router.post("/recognize")
 def recognize_face(image: UploadFile = File(...)):
     try:
-        return recognize_person(image)
+        result = recognize_person(image)
+        return JSONResponse(content=result, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-
+"""
 @router.post("/verify/{person_id}")
 def verify_face(person_id: str,image: UploadFile = File(...)):
     try:
-        return verify_person(person_id, image)
+        result = verify_person(person_id, image)
+        return JSONResponse(content=result, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
-
+"""
 
 @router.get("/persons")
 def get_persons():
     try:
-        return list_persons()
+        result = list_persons()
+        return JSONResponse(content=result, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
@@ -53,6 +57,7 @@ def get_persons():
 @router.delete("/persons/{person_id}")
 def remove_person(person_id: str):
     try:
-        return delete_person(person_id)
+        result = delete_person(person_id)
+        return JSONResponse(content=result, status_code=200)
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
